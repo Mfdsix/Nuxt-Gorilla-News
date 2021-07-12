91b111e8277796b3487fae017db45602be3c4c99
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-07-12 09:34:06 +0700

    enable ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -60,6 +60,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // enable page transition
+  layoutTransition: {
+    name: 'layout',
+    mode: 'out-in'
   },
 
   // proxy configuration
